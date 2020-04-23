3242209effe6b9ab5e309a9fc66b9aaee4476f10
Zelalem Alemu <leoxanigm@gmail.com>
2020-04-23 13:24:40 +0200

    Started task input form

--- src/App.js
+++ src/App.js
@@ -1,44 +1,50 @@
-import React, { Fragment } from 'react';
-import { Segment, Grid, Message, Icon } from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { Segment, Grid, Icon } from 'semantic-ui-react';
 import AddTask from './features/AddTask.jsx';
 import TaskList from './features/TaskList.jsx';
 
-function App() {
-  return (
-    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-      <Grid.Column style={{ maxWidth: 660 }}>
-        <Segment raised>
-          <Grid.Column>
-            <Grid divided='vertically'>
-              <Grid.Row>
-                <Grid.Column>
-                  <h1>ToDo</h1>
-                </Grid.Column>
-              </Grid.Row>
-              <Grid.Row>
-                <Grid.Column>
-                  <AddTask />
-                </Grid.Column>
-              </Grid.Row>
-              <Grid.Row>
-                <Grid.Column>
-                  <TaskList />
-                </Grid.Column>
-              </Grid.Row>
-              <Grid.Row>
-                <Grid.Column>
-                  <p className='ui text grey left aligned grid'>
-                    <Icon name='copyright outline' />
-                    Zelalem Alemu
-                  </p>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Grid.Column>
-        </Segment>
-      </Grid.Column>
-    </Grid>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Grid
+        textAlign='center'
+        style={{ height: '100vh' }}
+        verticalAlign='middle'
+      >
+        <Grid.Column style={{ maxWidth: 660 }}>
+          <Segment raised>
+            <Grid.Column>
+              <Grid divided='vertically'>
+                <Grid.Row>
+                  <Grid.Column>
+                    <h1>ToDo</h1>
+                  </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                  <Grid.Column>
+                    <AddTask />
+                  </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                  <Grid.Column>
+                    <TaskList />
+                  </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                  <Grid.Column>
+                    <p className='ui text grey left aligned grid'>
+                      <Icon name='copyright outline' />
+                      Zelalem Alemu
+                    </p>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Grid.Column>
+          </Segment>
+        </Grid.Column>
+      </Grid>
+    );
+  }
 }
 
 export default App;
--- src/features/AddTask.jsx
+++ src/features/AddTask.jsx
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
-import { Segment, Form } from 'semantic-ui-react';
+import { Segment, Form, Button } from 'semantic-ui-react';
 
 class AddTask extends Component {
+
+  state = {
+    task: ''
+  };
+
+  handlerOnStateChange = ({target: {value}}) => {
+    this.setState({
+      task: value
+    })
+  }
+
+  handlerOnSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.task);
+  }
+
   render() {
+    const {task} = this.state;
+
     return (
       <Segment>
-        <Form>
-          
+        <Form onSubmit={this.handlerOnSubmit}>
+          <Form.Field>
+            <input
+              type='text'
+              name='taskInput'
+              placeholder='Add Task'
+              value={task}
+              onChange={this.handlerOnStateChange}
+            />
+          </Form.Field>
+          <Button type='submit' content='Add Task'/>
         </Form>
       </Segment>
-    )
+    );
   }
 }
 
