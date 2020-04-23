59cf5d0413d27352679abb27af9f5610cc423138
Zelalem Alemu <leoxanigm@gmail.com>
2020-04-23 12:47:41 +0200

    Second Commit

---
+++ src/features/AddTask.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Segment, Form } from 'semantic-ui-react';
+
+class AddTask extends Component {
+  render() {
+    return (
+      <Segment>
+        <Form>
+          
+        </Form>
+      </Segment>
+    )
+  }
+}
+
+export default AddTask;
